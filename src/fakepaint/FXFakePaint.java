cfb419dbfffd2ab94e39accd3b014ab45b6fefc6
zekeb <zekeb@DESKTOP-GT6N9DV.hitronhub.home>
2018-02-18 03:52:03 -0600

    Allows you to erase as well as draw

--- src/fakepaint/FXFakePaint.java
+++ src/fakepaint/FXFakePaint.java
@@ -1,14 +1,19 @@
 package fakepaint;
 
+import java.awt.Paint;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;;
 
@@ -19,8 +24,10 @@ public class FXFakePaint extends Application{
 		
 		BorderPane pane = new BorderPane();
 		VBox vBox = new VBox();
-		
+		//the label for the scroll bar
 		Text scrollBarLabel = new Text("Size of Circle");
+		
+		CheckBox erasure = new CheckBox();
 		
 		//Create and define the values of a scrollbar
 		ScrollBar sc = new ScrollBar();
@@ -30,22 +37,32 @@ public class FXFakePaint extends Application{
 		
 		ColorPicker colorPicker = new ColorPicker();
 		//what you draw on
-		Canvas canvas = new Canvas(400,400);		
+		Canvas canvas = new Canvas(400,400);	
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		//allows you to draw on a mouse click
-		canvas.setOnMouseDragged(e -> {
-			
-			if(e.getButton() == MouseButton.PRIMARY ) {
-			gc.setStroke(colorPicker.getValue());
-			gc.setFill(colorPicker.getValue());
-			gc.fillOval(e.getX() , e.getY(), sc.getValue(), sc.getValue());
-			gc.strokeOval(e.getX(), e.getY(), sc.getValue(), sc.getValue());
+		//Creates a circle on mouse click
+		canvas.setOnMousePressed(e ->{		
+		if(e.getButton() == MouseButton.PRIMARY && !erasure.isSelected()) {
+				drawCircle(colorPicker,gc,e,sc);		
+			}
+		else {
+			erase(gc,e,sc);
 			
 		}
 			
 		});
+		//when the mouse is dragged it creates circles following that line
+		canvas.setOnMouseDragged(e -> {
+			
+			if(e.getButton() == MouseButton.PRIMARY && !erasure.isSelected()) {
+				drawCircle(colorPicker,gc,e,sc);
+		}
+			else {
+				erase(gc,e,sc);
+			}
+			
+		});
 		
-		vBox.getChildren().addAll(colorPicker,scrollBarLabel,sc);
+		vBox.getChildren().addAll(colorPicker,scrollBarLabel,sc,erasure);
 		
 		pane.setCenter(canvas);
 		pane.setLeft(vBox);
@@ -61,6 +78,16 @@ public class FXFakePaint extends Application{
 	public static void main(String[] args) {
 		
 		Application.launch(args);
+	}
+	public void drawCircle(ColorPicker colorPicker,GraphicsContext gc,MouseEvent e,ScrollBar sc) {
+		gc.setFill(colorPicker.getValue());
+		gc.fillOval(e.getX() , e.getY(), sc.getValue(), sc.getValue());
+	}
+	
+	public void erase(GraphicsContext gc,MouseEvent e,ScrollBar sc) {
+		gc.setFill(Color.WHITE);
+		gc.fillOval(e.getX() , e.getY(), sc.getValue(), sc.getValue());
+
 	}
 	
 }
